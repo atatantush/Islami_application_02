f06761b9d975656aba3377698e29587486b2f743
atatantush <atatantush@gmail.com>
2021-11-01 00:48:52 +0200

    Finish Home with widgets for each Tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:islami_02/HomeScreen.dart';
 
-void main() {}
+void main() {
+  runApp(MainApplication());
+}
+
+class MyThemeData {
+  static const Color primaryColor = Color.fromRGBO(183, 147, 95, 1.0);
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: Color.fromRGBO(183, 147, 95, 1.0),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+          color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.white,
+    ),
+  );
+  static final ThemeData darkTheme = ThemeData(
+    primaryColor: Color.fromRGBO(20, 26, 46, 1.0),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+          color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.yellow,
+      unselectedItemColor: Colors.white,
+    ),
+  );
+}
 
 class MainApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Islami',
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darkTheme,
       routes: {
         HomeScreen.routeName: (buildContext) => HomeScreen(),
       },
